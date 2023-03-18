fa10a5b4a469070ad4c020d89dadbdf4d765d0b4
Akihiro Suzuki <28677705+akhrszk@users.noreply.github.com>
2023-03-18 14:10:40 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from "react";
+import "./App.css";
 
 function App() {
+  const [inputJSON, setInputJSON] = useState("");
+
+  const formattedJSON = useMemo(() => {
+    try {
+      const parsedJSON = JSON.parse(inputJSON);
+      return JSON.stringify(parsedJSON, null, 2);
+    } catch (error) {
+      return null;
+    }
+  }, [inputJSON]);
+
+  const error = useMemo(() => {
+    if (formattedJSON === null && inputJSON.trim() !== "") {
+      try {
+        JSON.parse(inputJSON);
+      } catch (error) {
+        return error.message;
+      }
+    }
+    return null;
+  }, [inputJSON, formattedJSON]);
+
+  const handleJSONInput = (event) => {
+    setInputJSON(event.target.value);
+  };
+
+  const copyToClipboard = () => {
+    if (formattedJSON) {
+      navigator.clipboard.writeText(formattedJSON).then(
+        () => {
+          alert("整形済みJSONがクリップボードにコピーされました。");
+        },
+        (err) => {
+          console.error("クリップボードへの書き込みに失敗しました: ", err);
+        }
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>JSON整形ツール</h1>
+      <div>
+        <h3>JSON入力:</h3>
+        <textarea
+          rows="10"
+          cols="60"
+          value={inputJSON}
+          onChange={handleJSONInput}
+        ></textarea>
+      </div>
+      {error && (
+        <div>
+          <h3>エラー:</h3>
+          <p style={{ color: "red" }}>{error}</p>
+        </div>
+      )}
+      <div>
+        <h3>整形済みJSON:</h3>
+        <div className="json-output">
+          <pre>{formattedJSON}</pre>
+          <button className="copy-button" onClick={copyToClipboard}>
+            クリップボードにコピー
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
